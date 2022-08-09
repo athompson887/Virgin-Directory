d2927db99f8c73073ca6c5e4e3549cddec402fec
Andrew Thompson <andrew.thompson@firstchoice-cs.co.uk>
2022-08-09 08:59:10 +0100

    tidies

--- app/src/main/java/com/athompson/virgin/networking/AuthInterceptor.kt
+++ app/src/main/java/com/athompson/virgin/networking/AuthInterceptor.kt
@@ -1,16 +1,13 @@
 package com.athompson.virgin.networking
 
-import com.athompson.virgin.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var req = chain.request()
-        // DONT INCLUDE API KEYS IN YOUR SOURCE CODE
-        // Edit (or add) a gradle.properties file in your project root
-        // and add the API_KEY there!
-        val url = req.url.newBuilder().addQueryParameter("APPID", "GGGG").build()//todo remove key
+        //Not using in this project
+        val url = req.url.newBuilder().addQueryParameter("APPID", "TOKEN").build()
         req = req.newBuilder().url(url).build()
         return chain.proceed(req)
     }
--- app/src/main/java/com/athompson/virgin/networking/RoomApi.kt
+++ app/src/main/java/com/athompson/virgin/networking/RoomApi.kt
@@ -1,17 +1,10 @@
 package com.athompson.virgin.networking
 
-import com.athompson.virgin.PEOPLE
 import com.athompson.virgin.ROOMS
-import com.athompson.virgin.data.Person
 import com.athompson.virgin.data.Room
-import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
 
 interface RoomApi {
-
     @GET(ROOMS)
     suspend fun getRooms(): List<Room>
-    @GET(PEOPLE)
-    suspend fun getPeople(): List<Person>
 }

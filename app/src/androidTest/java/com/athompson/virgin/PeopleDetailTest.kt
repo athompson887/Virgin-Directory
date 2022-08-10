90ae92a99aecfb3df018b4a9377fdb42745cf1b3
athompson887 <46321983+andrewtfirstchoice@users.noreply.github.com>
2022-08-10 14:16:53 +0100

    final refinements
    remove unused colours, add ontent descriptions

--- app/src/androidTest/java/com/athompson/virgin/PeopleDetailTest.kt
+++ app/src/androidTest/java/com/athompson/virgin/PeopleDetailTest.kt
@@ -1,6 +1,8 @@
 package com.athompson.virgin
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -19,6 +21,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.dsl.module
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class PeopleDetailTest {
@@ -36,24 +41,33 @@ class PeopleDetailTest {
         }
     })
 
+    /*
+    *  This test does not work as hoped.  It does invoke the screen and check the field
+    *  but the field is not using the data that is passed in the view model.
+    *  I have some missing knowledge here - I cannot work out at the current time how to
+    *  make the test wait for the ViewModel to return its result before binding the data
+    *     */
     @Test
     fun testPersonDetail() {
         fragmentViewModel.selectedPerson.value = Person(
             avatar = "https://randomuser.me/api/portraits/women/21.jpg",
-            firstName = "Maggie",
-            lastName = "Brekke",
-            email =  "Crystel.Nicolas61@hotmail.com",
-            jobtitle =  "Future Functionality Strategist",
-            favouriteColor =  "pink",
+            firstName = "Andrew",
+            lastName = "Thompson",
+            email =  "a.thompson887@btinternet.com",
+            jobtitle =  "Hopeful developer for Virgin Money",
+            favouriteColor =  "Green",
             id = "1",
             createdAt = "2022-01-24T17:02:23.729Z")
 
         onScreen<PersonDetailScreen> {
-            firstNameText.matches { withText("Andrew") }
-         ///   lastNameText.matches { withText("Brekke") }
-        //    emailText.matches { withText("Crystel.Nicolas61@hotmail.com") }
-        }
 
+            fragmentViewModel.selectedPerson.observeForever {
+                firstNameText.matches { withText("Andrew") }
+            }
+
+         //  lastNameText.matches { withText("Brekke") }
+         //  emailText.matches { withText("Crystel.Nicolas61@hotmail.com") }
+        }
     }
 
 
@@ -61,5 +75,33 @@ class PeopleDetailTest {
         val firstNameText = KTextView { withId(R.id.first_name_text) }
         val lastNameText = KTextView { withId(R.id.last_name_text) }
         val emailText = KTextView { withId(R.id.email_text) }
+    }
+
+
+    /* Copyright 2019 Google LLC.
+   SPDX-License-Identifier: Apache-2.0 */
+    fun <T> LiveData<T>.getOrAwaitValue(
+        time: Long = 2,
+        timeUnit: TimeUnit = TimeUnit.SECONDS
+    ): T {
+        var data: T? = null
+        val latch = CountDownLatch(1)
+        val observer = object : Observer<T> {
+            override fun onChanged(o: T?) {
+                data = o
+                latch.countDown()
+                this@getOrAwaitValue.removeObserver(this)
+            }
+        }
+
+        this.observeForever(observer)
+
+        // Don't wait indefinitely if the LiveData is not set.
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return data as T
     }
 }
